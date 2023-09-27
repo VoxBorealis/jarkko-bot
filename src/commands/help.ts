6ecf8156eff6ea3e05e2a5406c2d4bdd641bde7a
Vox Borealis <uusitalo.y@gmail.com>
2023-09-27 20:17:21 +0300

    add cringe /help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,11 +6,11 @@ export const data = new SlashCommandBuilder()
 
 export const execute = async (interaction: CommandInteraction) => {
  const reply = `
-Jarkko DJ tässä terve teille kaikille, yo! 
-Laittakaa musapyyntöi hei nii etin ne Youtubesta ja laitan soimaa! 
-Täl hetkel toimii /play ja /skip. 
-(En oo tosiaan aina paikalla sit vielä, ei oo vakkarisopparia bro)
-(oon vähä cringe poju, hihihi :3 mjäyyyy)
+😎 Jarkko DJ tässä terve teille kaikille, yo! 😎
+Laittakaa musapyyntöi hei nii etin ne Youtubesta ja laitan soimaa! 🔥
+Täl hetkel toimii vaan /play ja /skip. 🤦
+(En oo tosiaan aina paikalla sit vielä, ei oo vakkarisopparia bro) 🙅
+(oon vähä cringe poju, hihihi :3 mjäyyyy) 😻
 `;
  return interaction.reply(reply)
 }
